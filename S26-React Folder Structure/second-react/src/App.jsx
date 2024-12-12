7d7b82ea55867ad192be9c06ee4ac5997e9a152f
surendra355 <surendravarma571@gmail.com>
2024-12-12 18:17:24 +0530

    Add files via upload

---
+++ S26-React Folder Structure/second-react/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     <h1>Pushpa 2: The Rule</h1>
+     <hr />
+     <p id='paragraph'>Pushpa 2: The Rule is a 2024 Indian Telugu-language action drama film[6] written and directed by Sukumar, and produced by Mythri Movie Makers, in association with Sukumar Writings. The film stars Allu Arjun in the titular role, alongside Rashmika Mandanna, Fahadh Faasil, Jagapathi Babu, Dhananjaya, Rao Ramesh, Sunil and Anasuya Bharadwaj. It is the second instalment in the Pushpa film series and the sequel to the 2021 film Pushpa: The Rise.
+
+The sequel was officially announced a few days prior to the original film's release in December 2021, under Pushpa 2, while the subtitle Pushpa 2: The Rule was announced in August 2022.[7][8] 10% of the film's footage was initially shot back-to-back with the original film. However, Sukumar decided to alter the story, which led to the film's principal photography commencing in October 2022. The film has music composed by Devi Sri Prasad, cinematography handled by Mirosław Kuba Brożek and editing by Naveen Nooli. Made on a budget of ₹400–500 crore,[3][9] the film is one of the most expensive Indian films. With a runtime of 200 minutes,[10] it is one of the longest Indian films ever made.
+
+Pushpa 2: The Rule was released worldwide on 5 December 2024 in standard, IMAX, 4DX, D-Box and PVR ICE formats. Setting several box-office records, it has earned over ₹1,032 crore (US$120 million) worldwide in its first seven days, becoming one of the highest-grossing Indian films, third highest-grossing Telugu film of all time, and the second highest-grossing Indian film of 2024.[11]</p>
+    <h2>Allu Arjun as Pushpa Raj</h2>
+     <img src='https://i.ytimg.com/vi/5zzudcr61s8/maxresdefault.jpg' width={1060}></img>
+     <h2>Rashmika Mandanna as Srivalli, Pushpa's wife</h2>
+     <img src='https://thetelugufilmnagar.com/storage/2024/04/Stunning-Look-1.webp'></img>
+     <h2>Fahadh Faasil as SP Bhanwar Singh Shekhawat IPS</h2>
+     <img src='https://i.ytimg.com/vi/0trlP_I14gs/maxresdefault.jpg' width={1070}></img>
+    </div>
+  )
+}
+
+export default App
